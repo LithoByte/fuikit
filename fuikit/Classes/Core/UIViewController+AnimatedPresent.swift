75fe219e24ac033738c052a64bd86f3f65405061
Calvin Collins <calvin.john.collins@gmail.com>
2021-10-18 16:22:44 -0700

    added animated presenting functions

---
+++ fuikit/Classes/Core/UIViewController+AnimatedPresent.swift
@@ -0,0 +1,37 @@
+//
+//  UIViewController+AnimatedPresent.swift
+//  fuikit
+//
+//  Created by Calvin Collins on 10/18/21.
+//
+
+import Foundation
+import LithoOperators
+
+public extension UIViewController {
+    func animateTopDown(duration: Double = 1.0) -> (UIView) -> Void {
+        return { [weak self] view in
+            let size = view.frame.size
+            let center = self?.view.center ?? CGPoint(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY)
+            view.frame = CGRect(x: center.x, y: -1 * size.height, width: size.width, height: size.height)
+            let dy = center.y - view.center.y
+            self?.view.addSubview(view)
+            DispatchQueue.main.async {
+                UIView.animate(withDuration: duration, animations: view *> shiftViewDown(by: dy))
+            }
+        }
+    }
+    
+    func animateBottomUp(duration: Double = 1.0) -> (UIView) -> Void {
+        return { [weak self] view in
+            let size = view.frame.size
+            let center = self?.view.center ?? CGPoint(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY)
+            view.frame = CGRect(x: center.x, y: UIScreen.main.bounds.height, width: size.width, height: size.height)
+            let dy = view.center.y - center.y
+            self?.view.addSubview(view)
+            DispatchQueue.main.async {
+                UIView.animate(withDuration: duration, animations: view *> shiftViewUp(by: dy))
+            }
+        }
+    }
+}
