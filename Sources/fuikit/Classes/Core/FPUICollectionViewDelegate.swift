8da688e3b0099011375e88bfd1e9a51db75ea6c9
Calvin Collins <calvin.john.collins@gmail.com>
2022-01-27 17:52:51 -0500

    both spm and pods build

--- Sources/fuikit/Classes/Core/FPUICollectionViewDelegate.swift
+++ Sources/fuikit/Classes/Core/FPUICollectionViewDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 open class FPUICollectionViewDelegate: NSObject, UICollectionViewDelegate {
     public var onDidHighlightItemAt: ((UICollectionView, IndexPath) -> Void)?
--- Tests/fuikitTests/fuikitTests.swift
+++ Tests/fuikitTests/fuikitTests.swift
@@ -1,11 +1,2 @@
 import XCTest
 @testable import fuikit
-
-final class fuikitTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(fuikit().text, "Hello, World!")
-    }
-}
